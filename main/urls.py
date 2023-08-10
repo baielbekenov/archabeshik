57bdc9ae6247e37fedc6face452fc90458ec6e1e
baiel bekenov <baelbekbolotovb12356@gmail.com>
2023-08-10 14:37:25 +0600

    added admin

--- main/urls.py
+++ main/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('project.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', views.obtain_auth_token)
